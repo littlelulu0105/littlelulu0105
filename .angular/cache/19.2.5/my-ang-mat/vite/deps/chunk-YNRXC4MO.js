ac092a7f3d9b1e4f89681be4f0062713f0f07ab0
YooKyung Han <yookyunghan@example.com>
2025-08-15 10:53:20 -0700

    Auto-generated commit

--- .angular/cache/19.2.5/my-ang-mat/vite/deps/chunk-YNRXC4MO.js
+++
@@ -1,9 +0,0 @@
-// node_modules/@angular/cdk/fesm2022/array-88e1bec3.mjs
-function coerceArray(value) {
-  return Array.isArray(value) ? value : [value];
-}
-
-export {
-  coerceArray
-};
-//# sourceMappingURL=chunk-YNRXC4MO.js.map
--- Ang/my-ang-mat-web/dist/out-tsc/app/app.routes.server.js
+++
@@ -1,7 +0,0 @@
-import { RenderMode } from '@angular/ssr';
-export const serverRoutes = [
-    {
-        path: '**',
-        renderMode: RenderMode.Prerender
-    }
-];
